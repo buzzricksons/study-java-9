d25d0fd9c77a1d6b62c5201e9e06d83b6f0b9bed
HyungCheol Kim <hcgoon@gmail.com>
2017-11-10 18:13:20 +0900

    add dropWhile, takeWhile

--- src/main/java/Sample01.java
+++ src/main/java/Sample01.java
@@ -1,2 +1,33 @@
+import java.util.stream.Stream;
+
 public class Sample01 {
+    public static void main(String[] args) {
+        takeWhile();
+        dropWhile();
+
+    }
+
+    public static void takeWhile () {
+        System.out.println("takeWhile 1:");
+        Stream.of("A is Java", "B is Groovy", "C is Kotlin", "D is Scala", "E is Python", "F is Go")
+                .takeWhile(s -> !s.contains("Scala"))
+                .forEach(System.out::println);
+
+        System.out.println("takeWhile 2:");
+        Stream.of(1, 2, 3, 4, 5, 6)
+                .takeWhile(i -> i < 3)
+                .forEach(System.out::println);
+    }
+
+    public static void dropWhile () {
+        System.out.println("dropWhile 1:");
+        Stream.of("A is Java", "B is Groovy", "C is Kotlin", "D is Scala", "E is Python", "F is Go")
+                .dropWhile(s -> !s.contains("Scala"))
+                .forEach(System.out::println);
+
+        System.out.println("dropWhile 2:");
+        Stream.of(1, 2, 3, 4, 5, 6)
+                .dropWhile(i -> i < 3)
+                .forEach(System.out::println);
+    }
 }
